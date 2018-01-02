a3974eeba5d2b3589531b8cb52478514eebd3dbf
ronantimmons <ronantimmons1988@gmail.com>
2018-01-02 15:36:48 +0000

    Initial checkin of mavin project

---
+++ mavin-demo/src/main/java/com/codebind/Student.java
@@ -0,0 +1,20 @@
+package com.codebind;
+
+/**
+ *  Author: Ronan Timmons
+ * Created: 20/12/17
+ * LUpdate: 02/01/18
+ */
+public class Student {
+
+    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
+
+    public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 50) return AttendanceGrade.POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance <= 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
+    }
+}
